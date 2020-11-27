b89cc0774c359f9745b7521e4d7b9b7a730cb111
abdoulsn <ablaye0m@gmail.com>
2020-11-27 21:57:57 +0100

    update

--- codes/prepare_data.py
+++ codes/prepare_data.py
@@ -1,29 +1,20 @@
-
-import numpy as np
 import pandas as pd
-import tensorflow as tf
-import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
-
-DATA_PATH = "data.json"
-SAVED_MODEL_PATH = "model.h5"
-LEARNING_RATE = 0.0001
 
 
 def load_data(data_path):
     """Chargé les données via csv .
 
-    : param data_path (str): chemin vers le csv
+     : param data_path (str): chemin vers le csv
      : retourne X un pandas dataframe: les predicteures
      : return y un pandas: la cibles
-
     """
     with open(data_path, "r") as fp:
         data = pd.read_csv(fp)
 
-    X = data.iloc[:,:-1]
-    y = data.iloc[:,-1]
-    print("Base totale chargé!")
+    X = data.iloc[:, :-1]
+    y = data.iloc[:, -1]
+    print("Échnatillon crées vous avez X et y!")
     return X, y
 
 
@@ -44,18 +35,14 @@ def echantillons(data_path, test_size=0.2, validation_size=0.2):
 
     # charge de la base
     X, y = load_data(data_path)
-
     # Extraction train, validation, test split
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size)
     X_train, X_validation, y_train, y_validation = train_test_split(X_train, y_train, test_size=validation_size)
 
-    # gestion des axes
-    # X_train = X_train[..., np.newaxis]
-    # X_test = X_test[..., np.newaxis]
-    # X_validation = X_validation[..., np.newaxis]
-
+    print("\n")
+    print("Echantillons X_train, y_train, X_validation, y_validation, X_test, y_test crées.")
     return X_train, y_train, X_validation, y_validation, X_test, y_test
+
 
 if __name__ == "__main__":
     echantillons(data_path="", test_size=0.2, validation_size=0.2)
-    
