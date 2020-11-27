d37d0964c6d6113295b6d2921cee377c185b5336
abdoulsn <ablaye0m@gmail.com>
2020-11-27 22:01:35 +0100

    update

--- codes/modele_de_cnn.py
+++ codes/modele_de_cnn.py
@@ -5,59 +5,16 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from prepare_data import *
 
-DATA_PATH = "data.json"
+DATA_PATH = "../data_out/rawdata.csv"
 SAVED_MODEL_PATH = "model.h5"
-EPOCHS = 40
-BATCH_SIZE = 32
+EPOCHS = 20
+BATCH_SIZE = 8
 PATIENCE = 5
-LEARNING_RATE = 0.0001
+LEARNING_RATE = 0.001
 
 
-def load_data(data_path):
-    """Loads training dataset from json file.
+X_train, y_train, X_validation, y_validation, X_test, y_test = echantillons(DATA_PATH, test_size=0.1, validation_size=0.1)
 
-    :param data_path (str): Path to json file containing data
-    :return X (ndarray): Inputs
-    :return y (ndarray): Targets
-
-    """
-    with open(data_path, "r") as fp:
-        data = json.load(fp)
-
-    X = np.array(data["MFCCs"])
-    y = np.array(data["labels"])
-    print("Training sets loaded!")
-    return X, y
-
-
-def prepare_dataset(data_path, test_size=0.2, validation_size=0.2):
-    """Creates train, validation and test sets.
-
-    :param data_path (str): Path to json file containing data
-    :param test_size (flaot): Percentage of dataset used for testing
-    :param validation_size (float): Percentage of train set used for cross-validation
-
-    :return X_train (ndarray): Inputs for the train set
-    :return y_train (ndarray): Targets for the train set
-    :return X_validation (ndarray): Inputs for the validation set
-    :return y_validation (ndarray): Targets for the validation set
-    :return X_test (ndarray): Inputs for the test set
-    :return X_test (ndarray): Targets for the test set
-    """
-
-    # load dataset
-    X, y = load_data(data_path)
-
-    # create train, validation, test split
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size)
-    X_train, X_validation, y_train, y_validation = train_test_split(X_train, y_train, test_size=validation_size)
-
-    # add an axis to nd array
-    X_train = X_train[..., np.newaxis]
-    X_test = X_test[..., np.newaxis]
-    X_validation = X_validation[..., np.newaxis]
-
-    return X_train, y_train, X_validation, y_validation, X_test, y_test
 
 
 def build_model(input_shape, loss="sparse_categorical_crossentropy", learning_rate=0.0001):
@@ -189,11 +146,4 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
-
-
-
-
-
 
